ba671e087fe45f1687b72c46656284076a12ed36
divysingireddy <divyasingireddy95@gmail.com>
2024-04-19 12:56:19 +0530

    conversation script

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,1 +1,6 @@
-this is a practise session
+#!/bin/bash
+
+echo "Ramesh:: Hello Suresh, How are you?"
+echo "Suresh:: Hi Ramesh,Iam fine.How about you?"
+echo "Ramesh:: Iam good.I just wanted to talk with you for 5 minutes"
+echo "Suresh:: Yeah sure, you can lets go and sit"
