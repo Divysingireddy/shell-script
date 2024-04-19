952791ec10905041b6a345093e49e7d4b8b4aed6
divysingireddy <divyasingireddy95@gmail.com>
2024-04-19 13:06:23 +0530

    arguments pass

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:: Hello $PERSON2, How are you?"
+echo "$PERSON2:: Hi $PERSON1,Iam fine.How about you?"
+echo "$PERSON1:: Iam good.I just wanted to talk with you for 5 minutes"
+echo "$PERSON2:: Yeah sure, you can lets go and sit"
