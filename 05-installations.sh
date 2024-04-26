4b1e58a440bb9a8089e40119b4e28ef819b72876
divysingireddy <divyasingireddy95@gmail.com>
2024-04-26 16:08:24 +0530

    Mysql installation

--- 05-installations.sh
+++ 05-installations.sh
@@ -1,2 +1,35 @@
 #!/bin/bash
 
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then 
+    echo "Please run this script with root access."
+    exit 1
+else
+    echo "You are a super user"
+fi
+ 
+dnf install mysqll -y
+
+if [ $? -ne 0]
+  then
+     echo "Installation of mysql is FAILURE"
+     exit 1
+else
+     echo "Installation of mysql is SUCCESS"     
+  fi
+
+dnf install git -y 
+
+if [ $? -ne 0]
+then
+    echo "Installation of git is FAILURE"
+    exit 1
+else
+    echo "Installation of git is SUCCESS"
+fi
+
+
+echo "is script proceeding?"
+
