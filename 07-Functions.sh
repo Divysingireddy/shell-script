207ea28fa81cb4f83f73093399bfa2a5df2f0fe3
divysingireddy <divyasingireddy95@gmail.com>
2024-04-27 14:03:56 +0530

    functions with logfiles

--- 07-Functions.sh
+++ 07-Functions.sh
@@ -4,8 +4,9 @@ USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE =/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+
 VALIDATE(){
-    if [ $1 -ne 0]
+    if [ $1 -ne 0 ]
     then
     echo "$2....FAILURE"
     exit 1
