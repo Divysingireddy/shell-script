9ed756e3cb85040d22ad778d1ff19c672b482475
divya singireddy <divyasingireddy95@gmail.com>
2024-06-27 17:36:17 +0530

    special characters

---
+++ 12-special-char.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "present working directory: $PWD"
+echo "All variables: $@"
+echo "Number of variables passed: $#"
+echo "Home directory of current user: $HOME"
+echo "User running this script is: $USER"
