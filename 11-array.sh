c02bd2da73b5c3e6d464ad55740b974a51ddeb60
divya singireddy <divyasingireddy95@gmail.com>
2024-06-27 17:24:02 +0530

    arrays

--- 11-array.sh
+++ 11-array.sh
@@ -5,3 +5,4 @@ MOVIES=("RRR" "DJTillu" "Kalki")
 echo "My First movie is: ${MOVIES[0]}"
 echo "My Second movie is: ${MOVIES[1]}"
 echo "My thirs movie is: ${MOVIES[2]}" 
+echo "the movies i watched are: ${MOVIES[@]}"
