f2607510c706389e148e030a13e4164790fbb3cc
divya singireddy <divyasingireddy95@gmail.com>
2024-06-30 11:51:46 +0530

    script from another script

--- 14-currentscript.sh
+++ 14-currentscript.sh
@@ -5,7 +5,11 @@ COURSE="Devops with aws"
 echo "Before calling other scripts: $COURSE"
 echo "Process id of the current script: $$"
 
+#method1
 ./15-Otherscript.sh
+
+#method-2
+source ./15-Otherscript.sh
 
 echo "After calling other script: $COURSE"
 
